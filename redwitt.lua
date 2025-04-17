28efd0e6ab0a2c829510ef154e35c9b57a842617
Redw1tt <83013895+Redw1tt@users.noreply.github.com>
2025-04-17 13:37:31 +0200

    Add files via upload

---
+++ redwitt.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Redwitt", [[Interface\Addons\SharedMedia_Redwitt\font\redwitt.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "BloodLust", [[Interface\Addons\SharedMedia_Redwitt\sound\bloodlust.ogg]])
+-- -----
+--   TEXTURE --
+-- -----
+
+LSM:Register("statusbar", "Redwitt", [[Interface\Addons\SharedMedia_Redwitt\statusbar\redwitt.tga]]) 
